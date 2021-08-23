f5d072470107a16be856aa7836a4ecfb80f7cc54
NezuShi <49164932+n3zushi@users.noreply.github.com>
2021-08-23 15:39:36 +0200

    Update shared.lua

--- PrinterRC_Ammo/lua/entities/ammo/shared.lua
+++ PrinterRC_Ammo/lua/entities/ammo/shared.lua
@@ -8,8 +8,6 @@ ENT.Spawnable = true
 
 function ENT:SetupDataTables()
 
-    self:NetworkVar("Int" , 1 , "RCells")
-    self:NetworkVar( "Int", 1, "MoneyAmount" )
     self:DTVar("Entity",1,"nezushi_ent")
 
 end
